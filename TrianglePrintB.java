55857c7924398260b4c1295cde5068e022634b45
Macwinner1 <ifechukwuw@gmail.com>
2025-04-20 10:50:53 +0100

    firstCommit

---
+++ TrianglePrintB.java
@@ -0,0 +1,20 @@
+public class TrianglePrintB{
+public static void main(String[] args){
+
+for(int count = 1; count < 10; count++){
+
+for(int count1 = 1; count1 <= count; count1++){
+System.out.print(' ');
+for(int count2 = count1; count2 < count1 ; count2++){
+System.out.print('*');
+}
+
+}
+System.out.println();
+
+}
+
+
+}
+
+}
