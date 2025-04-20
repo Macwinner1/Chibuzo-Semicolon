ce88659553b6c42b79bf354f89d1d5fcb6734d19
Okafor Ifechukwu Chinemelum <127173998+Macwinner1@users.noreply.github.com>
2025-04-20 11:46:40 +0100

    Update TrianglePrintB.java

--- TrianglePrintB.java
+++ TrianglePrintB.java
@@ -1,12 +1,12 @@
 public class TrianglePrintB{
 public static void main(String[] args){
 
-for(int count = 1; count < 10; count++){
+for(int count = 1; count <= 10; count++){
 
-for(int count1 = 1; count1 <= count; count1++){
-System.out.print(' ');
-for(int count2 = count1; count2 < count1 ; count2++){
+for(int count1 = count; count1 <= 10; count1++){
 System.out.print('*');
+for(int count2 = 1; count2 < count ; count2++){
+System.out.print(' ');
 }
 
 }
