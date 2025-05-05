77b1dfbcde3ce5f3c68aa39ebdedb88ee675f96d
Macwinner1 <ifechukwuw@gmail.com>
2025-05-05 12:52:17 +0100

    Array score first commit

---
+++ ArrayScores.java
@@ -0,0 +1,24 @@
+public class ArrayScores{
+public static void main(String[] args){
+
+int[] scores = new int[5];
+scores[0] = 22;
+scores[1] = 51;
+scores[2] = 38;
+scores[3] = 23;
+scores[4] = 48;
+
+int index0 = scores[0];
+int index1 = scores[1];
+int index2 = scores[2];
+int index3 = scores[3];
+int index4 = scores[4];
+
+System.out.print(index0 + index1 + index2 + index3 + index4);
+
+System.out.println(index0 + index1 + index2 + index3 + index4);
+
+
+}
+
+}
