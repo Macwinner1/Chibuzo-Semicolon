728882068a3bef73f82dde1c9224c5dd3d150035
Macwinner1 <ifechukwuw@gmail.com>
2025-05-12 00:55:48 +0100

    final commit on my working Driller function

--- DrillerFunctions.java
+++ DrillerFunctions.java
@@ -4,31 +4,31 @@ public static int testDriller(int number){
 int result = 0;
 int price = 0;
 
-if(number >= 1 || number <= 4)
+if(number >= 1 && number <= 4)
 	price = 2000;
 	result = price * number;
 
-if(number >= 5 || number <= 9)
+if(number >= 5 && number <= 9)
 	price = 1800;
 	result = price * number;
 
-if(number >= 10 || number <= 29)
+if(number >= 10 && number <= 29)
 	price = 1600;
 	result = price * number;
 
-if(number >= 30 || number <= 49)
+if(number >= 30 && number <= 49)
 	price = 1500;
 	result = price * number;
 
-if(number >= 50 || number <= 99)
+if(number >= 50 && number <= 99)
 	price = 1300;
 	result = price * number;
 
-if(number >= 100 || number <= 199)
+if(number >= 100 && number <= 199)
 	price = 1200;
 	result = price * number;
 
-if(number >= 200 || number <= 499)
+if(number >= 200 && number <= 499)
 	price = 1100;
 	result = price * number;
 
