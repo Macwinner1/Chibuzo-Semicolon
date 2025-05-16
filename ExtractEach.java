8856a2383b5d03bb51a733e795a6837a461d0604
Macwinner1 <ifechukwuw@gmail.com>
2025-05-16 08:04:12 +0100

    just added if else condition to handle egde cases

--- ExtractEach.java
+++ ExtractEach.java
@@ -5,12 +5,15 @@ int len = numbers.length();
 int result = 0;
 int number = Integer.parseInt(numbers);
 
+if(number < 1 || number > 10000){
+System.out.print("This is invalid input"); 
+}
+else{
 for(int index = 0; index < len; index++){
 result += number % 10;
 number = number / 10;
-
 }
-
+}
 return result;
 }
 
