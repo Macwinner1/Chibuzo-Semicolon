72d802da9714d9c206e4eebc259355ff99cfc131
Macwinner1 <ifechukwuw@gmail.com>
2025-06-27 00:05:26 +0100

    bank app with test cases

---
+++ BankApp/src/Account/Account.java
@@ -0,0 +1,14 @@
+package Account;
+
+public class Accounts {
+    private String pin;
+    private int balance;
+
+    public void Accounts(String pin) {
+        this.pin = pin;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
